0ca2595bdd87f668478a6cc1968b632d14ef0e12
Fernando Correa de Oliveira <fernandocorrea@gmail.com>
2012-02-09 00:24:16 -0200

    Pouca coisa

--- jsgrid.pl
+++ jsgrid.pl
@@ -3,6 +3,8 @@
 use Mojolicious::Lite;
 
 my %agent;
+my @jobs;
+my %jnames;
 
 websocket "/agent/:aid" => sub{
    my $self = shift;
@@ -13,7 +15,19 @@ websocket "/agent/:aid" => sub{
    });
 };
 
+any "/add/job" => sub {
+   my $self = shift;
+   my $jname           = $self->param("jname");
+   $jnames{ $jname } ||= $self->param("code") || die "no job code";
+   push @jobs, $jname;
+   $self->render_json({status => "OK"});
+};
+
 get "/job/:jid" => sub {
+   my $self = shift;
+   my $jname = $self->stash->{jid};
+   return $self->render(text => $jnames{ $jname }) if exists $jnames{ $jname };
+   $self->render(text => "404 - Not Found", status => 404);
 };
 
 app->start;
