5f39d5de19f1240d82badd3ea27e47ebe1b035e5
Fernando Correa de Oliveira <fernandocorrea@gmail.com>
2012-02-17 15:43:52 -0200

    Acertando o Agent manager

--- lib/jsgrid/Agent.pm
+++ lib/jsgrid/Agent.pm
@@ -2,18 +2,26 @@ package jsgrid::Agent;
 
 use Moose;
 use Moose::Util::TypeConstraints;
+use DateTime;
 
 with 'Mongoose::Document' => { -pk => [ qw/aid/ ]  } ;
 
 my $encrease_percent = .5;
 my $decrease_percent = .1;
 
-has 'aid'             => ( is => 'ro', isa => 'Str', required => 1 ) ;
 has 'runned'          => ( is => 'rw', isa => 'Mongoose::Join[jsgrid::Run]' ) ;
 has 'running'         => ( is => 'rw', isa => 'jsgrid::Run' ) ;
 has 'disconfiability' => ( is => 'ro', isa => 'Num', default => 0.1 ) ;
-has 'state'           => ( is => 'rw', isa => enum( [ qw|idle waiting running done| ]), default => 'idle' ) ;
+has 'state'           => ( is => 'rw', isa => enum( [ qw|initializing idle waiting
+running done| ]), default => 'initializing' ) ;
 has 'active'          => ( is => 'rw', isa => 'Bool', default => 1 ) ;
+has 'connected_date'  => ( is => 'rw', isa => 'DateTime', traits => [ qw/Raw/ ], default => sub{ DateTime->now }   ) ;
+
+sub aid{
+   my $self = shift;
+   $self->save if not exists $self->{ _id };
+   $self->_id
+}
 
 sub encrease_confiability { 
    my $self = shift;
--- lib/jsgrid/Agent/Manager.pm
+++ lib/jsgrid/Agent/Manager.pm
@@ -1,5 +1,14 @@
 package jsgrid::Agent::Manager;
+
 use Moose;
+Mongoose->load_schema(  search_path=>'jsgrid', shorten=>1 );
+use Mongoose;
+Mongoose->db( "jsgrid2" ) ;
+Mongoose->naming(  [ 'decamel','plural' ] );
+
+use MongoDB::OID;
+
+( undef ) = ( $Mongoose::singleton );
 
 use jsgrid::Agent;
 
@@ -10,10 +19,10 @@ sub agents {
    $self->agent_collection->find->all;
 } 
 
-sub add_agent {
+sub new_agent {
    my $self = shift;
-   my $aid  = shift;
-   my $new = jsgrid::Agent->new(aid => $aid);
+   my $new = $self->agent_collection->new;
+   $new->connected_date( DateTime->now ) ;
    $new->save;
    $new
 }
@@ -21,7 +30,30 @@ sub add_agent {
 sub get_agent {
    my $self = shift;
    my $aid  = shift;
-   $self->agent_collection->find_one( { aid => $aid } ); 
+   $self->agent_collection->find_one( $self->create_idobj( $aid ) ) 
+}
+
+sub create_idobj {
+   my $self = shift;
+   my $val  = shift;
+   MongoDB::OID->new(  value => $self->get_id_from( $val )  )
+}
+
+sub get_id_from {
+   my $self = shift;
+   my $aid  = shift;
+   if( defined ref($aid) ) {
+      if( ref($aid) eq "MongoDB::OID" ) {
+         return $aid->value;
+         if( $aid->can( "aid" ) ) {
+            return $aid->aid;
+         } else {
+            die "bla";
+         }
+      }
+   } else {
+      return $aid;
+   }
 }
 
 42
